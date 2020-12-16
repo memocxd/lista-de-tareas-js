9678514ae698126afa43689c8e40d408b2219637
Kevin Zurita <memocxd@gmail.com>
2020-12-15 23:37:02 -0600

    Termine la aplicacion de tareas

---
+++ README.md
@@ -0,0 +1,27 @@
+# webpack-estandar
+Configuración estándar de webpack para mis desarrollos
+
+## Nota
+Es necesario recontruir los modulos de node con el siguiente comando:
+
+```
+npm install
+```
+
+Para crear la configuración inicial del proyecto
+
+```
+npm init
+```
+
+Para crear nuevamente el DIST
+
+```
+npm run build
+```
+
+Para crear el servidor envivo de node
+
+```
+npm start
+```
---
+++ src/classes/todo.class.js
@@ -0,0 +1,22 @@
+export class Todo {
+    
+    static fromJson({id, tarea, completado, creado}) {
+        const tempTodo = new Todo(tarea);
+
+        tempTodo.id = id;
+        tempTodo.completado = completado;
+        tempTodo.creado = creado;
+
+        return tempTodo;
+    }
+
+
+    constructor(tarea) {
+        this.tarea = tarea;
+        this.id = new Date().getTime(); // 123456789123
+        this.completado = false;
+        this.creado = new Date();
+    }
+
+    
+}
